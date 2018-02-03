6e324fd2a589d415e52ee94847cd7f01b2297d86
ganeshrav <ganesh_dev@hotmail.com>
2018-02-03 10:38:52 -0500

    Made some changes to placeholders...

--- src/org/jwrench/asm/adapters/another_placeholder.java
+++ src/org/jwrench/asm/adapters/another_placeholder.java
@@ -1,3 +1,24 @@
+/*
+ This file is a part of the JWrench Project (https://github.com/HyperParam/JWrench).
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+ ASM (OW2 Consortium)
+ Copyright (c) 2000-2011 INRIA, France Telecom
+    All rights reserved.
+
+ */
 package org.jwrench.asm.adapters;
 
 public class another_placeholder {
